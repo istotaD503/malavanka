8eee3aa2aa7b086f8ff88df0d7e0450a1aa0edd0
Alex May <alexander.mazhavy@gmail.com>
2020-05-28 22:57:56 -0400

    setup paper js

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Paper from "./paper/Paper";
+
 
 function App() {
   return (
     <div className="App">
       <h1 style={{ color: "white" }}>Малюй па-беларуску!</h1>
+      <Paper></Paper>
     </div>
   );
 }
